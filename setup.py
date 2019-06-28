321acc25e40d11d55b40fbf8ce7f8387e5b3661d
Travis Brown <travis@bryx.com>
2019-06-28 12:17:28 -0400

    See if we can get setup.py to play nice with --home

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
+    name = 'apns',
+    description = 'A python library for interacting with the Apple Push Notification Service',
     author = 'David Jacobs',
     author_email = 'david@29.io',
-    description = 'A python library for interacting with the Apple Push Notification Service',
     download_url = 'https://github.com/djacobs/PyAPNs',
     license = 'unlicense.org',
-    name = 'apns',
-    py_modules = ['apns'],
-    scripts = ['apns-send'],
     url = 'http://29.io/',
-    version = '2.0.1',
+    version = '2.0.2',
+    setup_requires=[
+        'setuptools_scm'
+    ],
+    install_requires=[
+        'setuptools >= 0.7',
+    ],
+    zip_safe=False,
+
 )
